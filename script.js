b80be245483e1af7101ee9cff0c58b9cf9bfaf4d
Serg <bondserz@gmail.com>
2022-08-04 13:10:09 +0300

    lesson 24

--- script.js
+++ script.js
@@ -128,24 +128,222 @@
 // Lesson 22
 'use strict'
 
-let num = 50;
+// let num = 50;
 
-// while   (num<=55){
-//     console.log(num);
-//     num++;
+// // while   (num<=55){
+// //     console.log(num);
+// //     num++;
+// // }
+
+// // do 
+// // {console.log(num);
+// //     num++;
+// // }
+// // while (num <55);
+
+// for(let i = 1; i < 10; i++){
+//     if(i===6){
+//         // break
+//         continue
+//     }
+//     console.log(i);
 // }
 
-// do 
-// {console.log(num);
-//     num++;
-// }
-// while (num <55);
+// let i = 3;
 
-for(let i = 1; i < 10; i++){
-    if(i===6){
-        // break
-        continue
-    }
-    console.log(i);
-    num++;
+// while (i) {
+//   alert( i-- );
+// }
+
+// for (let i = 0; i < 3; i++){
+//     console.log(i);{
+//     for (let i = 0; i < 3; i++)
+//         console.log(i);
+//     }
+
+// }
+
+// let result = '';
+// const length =  7;
+
+// for (let i = 1; i <= length; i++){
+//     for (let j = 0; j < i; j++){
+//         result += '*'
+
+//     }
+
+//     result += '\n';
+
+// }
+
+// console.log(result)
+
+
+// for (let i = 0; i < 3; i++){
+//     console.log('First level: ${i)');
+//         for (let j = 0; j < 3; j++)
+//             console.log('Socond level: ${j}');
+//             for (let k = 0; k < 3; k++)
+//                 console.log(' Third level; ${k}');
+//             }
+//         }
+ 
+//  }
+
+// lesson 22
+// "use strict";
+
+// let num = 50;
+
+// // while(num <= 55){
+// //     console.log(num);
+// //     num++;
+// // }
+
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
+// // while(num <55);
+
+// for (let i = 1; i < 10; i++){
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+
+// }
+
+// for (let i = 0; i < 3; i++){
+//     console.log(i);
+//         for (let j = 0; j < 3; j++){
+//         console.log(j);{
+
+//         }
+//     }
+// }
+    
+// let result = "";
+// const length = 7;
+
+// for (let i = 1; i < length; i++){
+
+//     for (let j = 0; j < i; j++){
+//         result += "*";
+//     }
+//     result +='\n';
+// }
+
+// console.log(result);
+
+
+//  first: for (let i = 0; i < 3; i++){
+//     console.log('first level: i');
+//         for (let j = 0; j < 5; j++){
+//         console.log('Second level: ${j}');
+//             for (let k = 0; k < 3; k++){
+//                 if (k === 2) 
+//                 // continue
+//                 break first;
+                
+//                 console.log('Third level: ${k}');
+//             }        
+//     }
+// }
+// задача №1
+// for ( let i = 5; i < 11; i++){
+//     console.log(i)
+// }
+
+// //задача №2
+// for (let i = 20; i > 10; i--){
+//     if (i === 13) break;
+//     console.log(i);
+// }
+
+// // задача №3
+// for (let i = 0; i <= 10; i++){
+//     if (i % 2 === 0){
+//         console.log(i);
+
+//     }
+// }
+
+// задача №4
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// let k = 2;
+// while (k < 16){
+//     k++;
+//         if (k % 2 === 1)
+    
+//         console.log(k);
+    
+
+// }
+    // задача №5
+// const arrayOfNumbers = [];
+
+// for (let i = 5; i < 11; i++){
+//     arrayOfNumbers[i - 5] = i;
+// }
+// console.log(arrayOfNumbers)
+        
+
+// // задача 2.1
+// const arr = [3, 5, 8, 16, 20,23,50];
+// const result = [];
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//     result[i] = arr[i];
+// }
+
+// console.log(result);
+// return result;
+// lesson24
+
+const numberOfFilms= +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMoviesDB = {
+    count:  numberOfFilms,   
+    movies: {},
+    actors: {},
+    genres:{},
+    privat: false
+};
+
+for (let i = 0; i < 2; i++){
+    const a= prompt('Один из просмотреных фильмов?',''),
+          b= prompt('Как оцените его?','');
+
+          if (a != null && b != null && a != '' && b != ' && ' && a.length < 50) {
+            personalMoviesDB.movies[a] = b;
+            console.log ('done');
+          } else {
+            console.log('errror');
+            i--;
+          }
+
+        
 }
+if (personalMoviesDB.count < 10) {
+    console.log('Мало');
+    } else if (personalMoviesDB.count < 30 && personalMoviesDB.count >= 10){
+        console.log('Normalno');
+    } else if (personalMoviesDB.count >= 30){
+        console.log('ZBS');
+    }else { 
+        console.log("Errror");
+    }
+console.log(personalMoviesDB);
